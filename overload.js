9eaaa44816b112f3254ebf7a8202734476dfe2ae
Alexander Podlesnyi <gwebdzen@gmail.com>
2019-11-15 06:57:54 +0200

    Fix test name

--- overload.js
+++ overload.js
@@ -118,14 +118,14 @@ overload.foo = function(test = {}){
     console.log('Object',test);
 }
 overload.foo = function(test = 11, str = ''){
-    console.log('multypart',test,str);
+    console.log('multipart',test,str);
 }
 overload.foo = function(hello = '', str1, str2){
     console.log('Any arguments',hello, str1, str2);
 }
 
 overload.foo("Hello");
-    overload.foo(10);
-    overload.foo({thisis:"object"});
-    overload.foo(100,'World');
-    overload.foo('Hello','World','Friends');
+overload.foo(10);
+overload.foo({thisis:"object"});
+overload.foo(100,'World');
+overload.foo('Hello','World','Friends');
